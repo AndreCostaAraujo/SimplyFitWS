d2b1e4240e7559f89c05be102289adb3860e1034
AndreCostaAraujo <andre.araujo@outlook.com>
2018-05-11 10:20:50 -0300

    Simply

--- src/com/ad4cbr/andre/simplyfit/ConectaMySql.java
+++ src/com/ad4cbr/andre/simplyfit/ConectaMySql.java
@@ -3,22 +3,12 @@ package com.ad4cbr.andre.simplyfit;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
-import org.ini4j.Ini;
-import org.ini4j.IniPreferences;
-import org.ini4j.InvalidFileFormatException;
 
 public class ConectaMySql {
 	
-	private static String URL = "jdbc:mysql://simplyfit-mysql:3306/simplyfit";
-	private static String USER = "SFAdmin";
-	private static String PASS = "123qwer@";
+	private static final String URL = "jdbc:mysql://simplyfit-mysql:3306/simplyfit";
+	private static final String USER = "SFAdmin";
+	private static final String PASS = "123qwer@";
 	
 	public static Connection obtemConexao() throws SQLException{
 		try {
